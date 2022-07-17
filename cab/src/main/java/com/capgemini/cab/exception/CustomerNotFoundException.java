84055f4370790fc5f4469c3c99b6065ac2a12c04
Akshat <setutrivedi200@gmail.com>
2022-07-17 11:59:26 +0530

    customer module

--- cab/src/main/java/com/capgemini/cab/exception/CustomerNotFoundException.java
+++ cab/src/main/java/com/capgemini/cab/exception/CustomerNotFoundException.java
@@ -1,6 +1,36 @@
 package com.capgemini.cab.exception;
 
-public class CustomerNotFoundException extends Exception {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value=HttpStatus.NOT_FOUND)
+public class CustomerNotFoundException extends RuntimeException {
+	private String resourceName;
+	private String fieldName;
+	private long fieldvalue;
+
+	public CustomerNotFoundException(String resourceName, String fieldName, long id) {
+		super(String.format("%s not found with %s:'%s'",resourceName,fieldName,id));
+		this.resourceName = resourceName;
+		this.fieldName = fieldName;
+		this.fieldvalue = id;
+	}
+
+	public String getResourceName() {
+		return resourceName;
+	}
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public long getFieldvalue() {
+		return fieldvalue;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 
 	/**
 	 * 
